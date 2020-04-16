31c1dd8f683ff1aa6df2ef99efce6d4f82529986
gmahechas <gmahechas@ingenieros.com>
2020-04-15 20:44:21 -0500

    first commit

---
+++ 05-transpilation-with-babel/src/index.js
@@ -0,0 +1,7 @@
+import { sum } from './operations';
+
+const name = 'Gustavo';
+
+console.log(`Hello world ${name}`);
+console.log(sum(3,2));
+
---
+++ 06-html-loader-n-html-plugin/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      file: './index.html'
+    })
+  ]
+};
